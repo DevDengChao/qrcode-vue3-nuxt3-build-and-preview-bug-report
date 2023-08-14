49e9f15b63da90a9287e53b4d3f6dbb8b5daaf6b
DevDengChao <2325690622@qq.com>
2023-08-14 11:31:35 +0800

    feat: display qr code

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script>
+import QRCodeVue3 from "qrcode-vue3"
+
+export default defineComponent({
+  components: {
+    // Manually register component,
+    // otherwise "Failed to resolve component: QRCodeVue3. Hydration node mismatch."
+    QRCodeVue3
+  }
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <QRCodeVue3 value="hello world"/>
 </template>
